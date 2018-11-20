7a154c016bc0dd967c74a42e48d01024a4ddd14f
Harshad Reddy Nalla <harshadreddy16@gmail.com>
2018-11-20 11:33:50 -0500

    update notebook

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -10,29 +10,36 @@ sample_df = pd.read_csv('Involved_x_Spark_Sheet1.csv')
 # Mongodb
 client = MongoClient()
 db = client['test']
+r = re.compile('^\w+$')
 
 
-def refine(tweet_texts: list):
+def refine(tweet_texts: list, details):
+    name = details['Name'].lower()
+    name = name.split()
+    handle = details['Data Point'].replace('@', '')
+    name.append(handle)
     for i in range(len(tweet_texts)):
         if tweet_texts[i]:
-            tweet_texts[i] = tweet_texts[i].replace('"', '')
-            tweet_texts[i] = tweet_texts[i].replace('"', '')
-            tweet_texts[i] = tweet_texts[i].replace("'", '')
-            tweet_texts[i] = tweet_texts[i].replace('.', '')
-            tweet_texts[i] = tweet_texts[i].replace('&', '')
-            tweet_texts[i] = tweet_texts[i].replace(',', '')
-            tweet_texts[i] = tweet_texts[i].replace('‘', '')
-            tweet_texts[i] = tweet_texts[i].replace('’', '')
-            tweet_texts[i] = tweet_texts[i].replace('”', '')
-            tweet_texts[i] = tweet_texts[i].replace('“', '')
+            not_required = ['"', '"', "'", '.', '&', ',', '‘', '’', '”', '“', '#']
+            for nr in not_required:
+                tweet_texts[i] = tweet_texts[i].replace(nr, '')
+
             tweet_texts[i] = ' '.join(filter(lambda x: 'https' not in x, tweet_texts[i].split()))
             tweet_texts[i] = ' '.join(filter(lambda x: 'http' not in x, tweet_texts[i].split()))
             tweet_texts[i] = ' '.join(filter(lambda x: '@' not in x, tweet_texts[i].split()))
             for word in filter(lambda x: x[0] == '#', tweet_texts[i].split()):
                 tweet_texts[i] = tweet_texts[i].replace(word,
                                                         re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', word))
-            tweet_texts[i] = tweet_texts[i].replace('#', '')
+
+            for word in tweet_texts[i].split():
+                if not re.match(r, word):
+                    tweet_texts[i] = tweet_texts[i].replace(word, '')
+
             tweet_texts[i] = tweet_texts[i].lower()
+
+            for name_part in name:
+                tweet_texts[i] = tweet_texts[i].replace(name_part, '')
+
     return tweet_texts
 
 
@@ -59,7 +66,8 @@ for index, details in sample_df.iterrows():
         else:
             if tweet.get('id') not in checked_ids:
                 texts.append(tweet['full_text'])
-    texts = refine(texts)
+    texts = refine(texts, details)
+    texts = [text for text in texts if text]
     df = pd.DataFrame(texts, columns=["text"])
     csv_name = 'preprocessed_data\\' + details['Data Point'] + '_refined_tweets_.csv'
     df.to_csv(csv_name, index=False)
