a382e16513b293a822b308b051e8e583074a78bd
Harshad Reddy Nalla <harshadreddy16@gmail.com>
2018-12-05 03:15:30 -0500

    fix issues in data_preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -9,7 +9,7 @@ keys = json.load(open('config.json', 'r'))
 sample_df = pd.read_csv('Involved_x_Spark_Sheet1.csv')
 # Mongodb
 client = MongoClient()
-db = client['test']
+db = client[keys['mongo_db']]
 r = re.compile('^\w+$')
 
 
@@ -48,16 +48,7 @@ for index, details in sample_df.iterrows():
     print("For the User: ", details['Data Point'])
     texts = []
     checked_ids = []
-    # batch_total = db[details['Data Point']].find().count()
-    # csv_id = 0
-    # batch_size = 3000
-    # if batch_total < 3000:
-    #     batch_size = batch_total
-
-    # for batch_value in range(0, int(batch_total/batch_size)+1):
-    #     print("batch value", batch_value)
-    #     skip = batch_value*batch_size
-    tweets = db[details['Data Point']].find(no_cursor_timeout=True)
+    tweets = db[details['Data Point']].find({}, no_cursor_timeout=True)
     for tweet in tweets:
         if 'retweeted_status' in tweet:
             if tweet['retweeted_status'].get('id') not in checked_ids:
@@ -72,6 +63,5 @@ for index, details in sample_df.iterrows():
     csv_name = 'preprocessed_data\\' + details['Data Point'] + '_refined_tweets_.csv'
     df.to_csv(csv_name, index=False)
     tweets.close()
-    # csv_id += 1
     print("Completed PreProcessing for the data of: ", details['Data Point'])
 print("Pre-Processing completed!")
